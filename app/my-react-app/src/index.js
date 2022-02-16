bc76c89e164918f1767969698c1da74c2acd1093
Mohammed <ironmomochi@gmail.com>
2022-02-15 23:09:19 -0500

    Adding the react app and ESLint and prettier settings

---
+++ app/my-react-app/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './App'
+
+// eslint-disable-next-line
+ReactDOM.render(<h1> My App</h1>, document.getElementById('root'))
