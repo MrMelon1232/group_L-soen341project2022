03bcad2a491bcffd7c3e1a5fdc7c7bdf4c6afc91
MrMelon1232 <julien.phan18@gmail.com>
2022-02-15 21:17:02 -0500

    Revert "Adding React app"
    This reverts commit 34d7c00284d95c3877f2dadd8f573e35cd07ccbb.

--- my-app/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import App from './App'
-
-// eslint-disable-next-line
-ReactDOM.render(<h1> My App</h1>, document.getElementById('root'))
