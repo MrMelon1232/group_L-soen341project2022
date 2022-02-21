8696ce3b19eb3a0f6c5daa87ec5803273893e8b0
Mohammed <ironmomochi@gmail.com>
2022-02-21 02:46:06 -0500

    adding buttons for logging in and an admin button

--- my-app/react-app/src/components/Navbar/Navbar.jsx
+++ my-app/react-app/src/components/Navbar/Navbar.jsx
@@ -1,46 +1,77 @@
+import AccountCircle from '@mui/icons-material/AccountCircle'
+import { IconButton, Menu, MenuItem } from '@mui/material'
 import Button from '@mui/material/Button'
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-
 import MenuItems from './MenuItems'
 import './Navbar.css'
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { clicked: false }
+export default function Navbar() {
+  const [clicked, setClicked] = React.useState(false)
 
-    this.handleClick = () => {
-      this.setState((prevState) => ({ clicked: !prevState.clicked }))
-    }
+  const handleClick = () => {
+    setClicked(!clicked)
   }
 
-  render() {
-    const { clicked } = this.state
-
-    return (
-      <nav className="NavbarItems">
-        <h1 className="navbar-logo">
-          AmaBay
-          <i className="fab fa-react" />
-        </h1>
-        <div role="button" className="menu-icon" onClick={this.handleClick}>
-          <i className={clicked ? 'fas fa-times' : 'fas fa-bars'} />
-        </div>
-        <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
-          {/*Might not work as intended*/}
-          {MenuItems.map((item, index) => (
-            <li key={index}>
-              <Link className={item.cName} to={item.url}>
-                {item.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <Button>Sign Up</Button>
-      </nav>
-    )
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const handleClose = () => {
+    setAnchorEl(null)
   }
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  return (
+    <nav className="NavbarItems">
+      <h1 className="navbar-logo">
+        AmaBay
+        <i className="fab fa-react" />
+      </h1>
+      <div role="button" className="menu-icon" onClick={handleClick}>
+        <i className={clicked ? 'fas fa-times' : 'fas fa-bars'} />
+      </div>
+      <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+        {/*Might not work as intended*/}
+        {MenuItems.map((item, index) => (
+          <li index={index}>
+            <Link className={item.cName} to={item.url}>
+              {item.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Button>Sign Up</Button>
+      <Button>Log In</Button>
+      <IconButton
+        size="large"
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleMenu}
+        color="inherit"
+      >
+        <AccountCircle color="primary" />
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        keepMounted
+        elevation={0}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Admin Login</MenuItem>
+        <MenuItem onClick={handleClose}>Become a member of the team</MenuItem>
+      </Menu>
+    </nav>
+  )
 }
-
-export default Navbar
