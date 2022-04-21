a89d4af3d182e99fc7985f7de33b93683692a6bb
Mohammed <68031021+MohammedJava@users.noreply.github.com>
2022-04-21 16:09:02 -0400

    Optimize product details page

--- my-app/react-app/src/pages/Products/catalogSlice.ts
+++ my-app/react-app/src/pages/Products/catalogSlice.ts
@@ -22,6 +22,18 @@ export const fetchProductsAsync = createAsyncThunk<Product[]>(
   }
 )
 
+export const fetchProductAsync = createAsyncThunk<Product, number>(
+  'catalog/fetchProductAsync',
+  async (productId) => {
+    try {
+      return await agent.Catalog.details(productId)
+    } catch (error) {
+      console.log(error)
+    }
+    return undefined
+  }
+)
+
 export const catalogSlice = createSlice({
   name: 'catalog',
   initialState: productsAdapter.getInitialState({
@@ -39,6 +51,16 @@ export const catalogSlice = createSlice({
       state.productsLoaded = true
     })
     builder.addCase(fetchProductsAsync.rejected, (state) => {
+      state.status = 'idle'
+    })
+    builder.addCase(fetchProductAsync.pending, (state) => {
+      state.status = 'pendingFetchProduct'
+    })
+    builder.addCase(fetchProductAsync.fulfilled, (state, action) => {
+      productsAdapter.upsertOne(state, action.payload)
+      state.status = 'idle'
+    })
+    builder.addCase(fetchProductAsync.rejected, (state) => {
       state.status = 'idle'
     })
   },
